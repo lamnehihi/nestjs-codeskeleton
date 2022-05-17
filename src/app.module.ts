46b77c95e7b652310b299399de2b50cde735d912
liam-nguyen <thanhlamnguyenhuynh@gmail.com>
2022-05-17 21:24:49 +0700

    Config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// console.log('node_env', `.env.${process.env.NODE_ENV}`);
+// console.log('process.env', process.env);
+// console.log('process.env.MONGO_URI', process.env.MONGO_URI);
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
