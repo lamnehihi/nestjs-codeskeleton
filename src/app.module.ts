2ff70aa080381b4681589450a71483ca426e0def
liam-nguyen <thanhlamnguyenhuynh@gmail.com>
2022-05-17 21:30:31 +0700

    implement user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
-// console.log('node_env', `.env.${process.env.NODE_ENV}`);
-// console.log('process.env', process.env);
-// console.log('process.env.MONGO_URI', process.env.MONGO_URI);
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
     MongooseModule.forRoot(process.env.MONGO_URL),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
