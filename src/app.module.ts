89a5c5ce309e5cb2007f5e8d7f6cd5bc98b6f9d0
liam-nguyen <thanhlamnguyenhuynh@gmail.com>
2022-05-17 21:48:05 +0700

    implement auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { UserModule } from './user/user.module';
     }),
     MongooseModule.forRoot(process.env.MONGO_URL),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
